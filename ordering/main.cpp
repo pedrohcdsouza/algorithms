2e70da9479cd7b89ea591914d197273536ee36b7
Pedro Henrique Cardoso de Souza <pedrohcsousa8@hotmail.com>
2025-02-16 22:17:07 -0300

    Adding a main-file to calculate the duration of the methods.

--- ordering/main.cpp
+++ ordering/main.cpp
@@ -1,15 +1,36 @@
-#include "ordenacao.cpp"
 #include <iostream>
+#include <fstream>
+#include <vector>
+#include <chrono>
+#include "ordenacao.cpp"
 
 int main() {
-    int a[10] = {2,8,5,7,3,6,4,9,1,10};
-    std::cout << ordenado(a, 10);
-    merge_sort(a, 0, 10);
-    std::cout << ordenado(a, 10);
+    std::ifstream file("testes/caso01/exemplo-30000-1.txt");
 
-    for (int i = 0; i < 5; ++i) {
-        std::cout << a[i] << " ";
+    if (!file.is_open()) {
+        std::cerr << "Error to open file." << std::endl;
+        return 1;
     }
+
+    int size;
+    file >> size;
+
+    std::vector<int> array(size);
+
+    for (int i = 0; i < size; ++i) {
+        file >> array[i];
+    }
+
+    file.close();
+
+    auto start = std::chrono::high_resolution_clock::now();
+
+    insercao(array.data(), size);
+
+    auto end = std::chrono::high_resolution_clock::now();
+
+    auto duration = std::chrono::duration_cast<std::chrono::microseconds>(end - start);
+    std::cout << "Time taken by function: " << duration.count() << " microseconds" << std::endl;
 
     return 0;
 }
